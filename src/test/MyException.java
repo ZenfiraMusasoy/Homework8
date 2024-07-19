9d1aad9dbdd923e5ba776d8935138c78bca01ce4
Zenfira <zenfira.musasoy@gmail.com>
2024-07-19 15:51:54 +0400

    create custom exception

--- src/test/MyException.java
+++ src/test/MyException.java
@@ -1,10 +1,14 @@
-//import java.util.Scanner;
-//
-//public class MyException {
-//
+package test;
+
+import java.util.Scanner;
+
+public class MyException extends RuntimeException {
+
+
+
 //        int[] myNumbers = {1, 2, 3};
 //        System.out.println(myNumbers[10]);
 //
 //        System.out.println("Something went wrong.");
-//}
-//
+}
+
